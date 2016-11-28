4ffe52f554d6a0b06d54547db46e2109775152ba
Aaron Vontell <vontell@mit.edu>
2016-11-28 11:12:07 -0500

    LED pulse test uses abstraction

--- samples/repository/pulse-test.py
+++ samples/repository/pulse-test.py
@@ -3,33 +3,19 @@
 # Date: 10-05-2016
 
 from artiq.experiment import *
+from rle.pipistrello import Board
 
 class LedPulseTest(EnvExperiment):
 	def build(self):
-		self.setattr_device("core")
-		self.setattr_device("ttl0")
-		self.setattr_device("led1")
-		self.setattr_device("led2")
-		self.setattr_device("led3")
-		self.setattr_device("led4")
+		
+		# Initialize the board
+		self.board = Board(self)
 
 	@kernel
 	def run(self):
-		self.core.reset()
 		
-		try:
-			self.core.break_realtime()
-			self.led1.pulse(250*ms)
-			self.led2.pulse(250*ms)
-			self.led3.pulse(250*ms)
-			self.led4.pulse(250*ms)
-			with parallel:
-				self.led1.pulse(500*ms)
-				self.led2.pulse(500*ms)
-				self.led3.pulse(500*ms)
-				self.led4.pulse(500*ms)
-		except RTIOUnderflow:
-			print_underflow()
+		self.board.reset()
+		self.board.led_test()
 
 def print_underflow():
     print("RTIO underflow occured")
