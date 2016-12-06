52c1e6a6d844e861afac09a1bbe9c1b885d9470b
Aaron Vontell <vontell@mit.edu>
2016-12-06 09:54:49 -0500

    Tab fixes

--- samples/repository/pipistrello-test.py
+++ samples/repository/pipistrello-test.py
@@ -22,24 +22,24 @@ class PipistrelloTest(EnvExperiment):
 		self.board.led_test()
         
         # Find the latency of this board
-        print("Finding board latency...")
+		print("Finding board latency...")
 		latency = self.board.find_latency(4 * us, 30, 50, 0)
 		print("Found latency: ", latency, " sec")
 		
-        # Start the pulse sequence
+		# Start the pulse sequence
 		self.board.get_core().break_realtime()
 		print("Before delay of 2 seconds: ", now_mu())
 		delay(2*s)
-        START = now_mu()
-        print("Start time: ", START)
+		START = now_mu()
+		print("Start time: ", START)
 		print("Starting pulse")
 		# pulse ttl = 0 for T = 4 us
 		self.board.pulse(0, 4 * us, 20)
-        print("Pulses placed. Done!")
+		print("Pulses placed. Done!")
         
-        # Start listening for rising edge events
-        print("Register rising edge event")
-        self.board.register_rising(pmt, next_pulse, START)
+		# Start listening for rising edge events
+		print("Register rising edge event")
+		self.board.register_rising(pmt, next_pulse, START)
 			
 def next_pulse():
 	print("Detected pulse!")
